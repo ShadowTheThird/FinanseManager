f419ef71c70a1a069db099c92bf5c06eeaa37f15
ShadowTheThird <michal.walach111@gmail.com>
2024-03-24 16:33:34 +0100

    Password help form

---
+++ Help.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FinanseManager
+{
+    partial class Help_Page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.TextBox();
+            this.username_label = new System.Windows.Forms.Label();
+            this.help = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.username.Location = new System.Drawing.Point(107, 145);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(298, 26);
+            this.username.TabIndex = 1;
+            this.username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.username_label.Location = new System.Drawing.Point(213, 122);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(91, 20);
+            this.username_label.TabIndex = 7;
+            this.username_label.Text = "Username";
+            // 
+            // help
+            // 
+            this.help.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.help.Location = new System.Drawing.Point(107, 256);
+            this.help.Name = "help";
+            this.help.Size = new System.Drawing.Size(120, 31);
+            this.help.TabIndex = 8;
+            this.help.Text = "Help";
+            this.help.UseVisualStyleBackColor = true;
+            this.help.Click += new System.EventHandler(this.help_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cancel.Location = new System.Drawing.Point(285, 256);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(120, 31);
+            this.cancel.TabIndex = 9;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // Help_Page
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 450);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.help);
+            this.Controls.Add(this.username_label);
+            this.Controls.Add(this.username);
+            this.Name = "Help_Page";
+            this.Text = "Help";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Help_Page_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.Button help;
+        private System.Windows.Forms.Button cancel;
+    }
+}
---
+++ Help.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinanseManager
+{
+    public partial class Help_Page : Form
+    {
+        Authenticator authenticator;
+        public Help_Page(Authenticator caller)
+        {
+            InitializeComponent();
+            authenticator = caller;
+        }
+
+        public void PreSet_Username(string name)
+        {
+            this.username.Text = name;
+        }
+
+        private string LoadCSV(string name)
+        {
+            string[] data = File.ReadAllLines("..\\..\\user_data.csv");
+            foreach (string line in data)
+            {
+                string[] split = line.Split(',');
+                if (split[0] == name)
+                {
+                    return split[2];
+                }
+            }
+            return null;
+        }
+
+        private void help_Click(object sender, EventArgs e)
+        {
+            if (this.username.Text.Length > 0)
+            {
+                string user = LoadCSV(this.username.Text);
+                if(user != null)
+                {
+                    MessageBox.Show(user, "Help Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("invalid username", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("please insert a valid username", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Help_Page_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            authenticator.Show();
+        }
+    }
+}
