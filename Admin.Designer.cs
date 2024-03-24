cfae2c74fa711d540b73fb5edabd02c78aafc424
ShadowTheThird <michal.walach111@gmail.com>
2024-03-24 18:11:38 +0100

    Admin form

---
+++ Admin.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FinanseManager
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcome_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // welcome_label
+            // 
+            this.welcome_label.AutoSize = true;
+            this.welcome_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.welcome_label.Location = new System.Drawing.Point(113, 34);
+            this.welcome_label.Name = "welcome_label";
+            this.welcome_label.Size = new System.Drawing.Size(137, 20);
+            this.welcome_label.TabIndex = 9;
+            this.welcome_label.Text = "Welcome Admin";
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.welcome_label);
+            this.Name = "Admin";
+            this.Text = "Admin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcome_label;
+    }
+}
