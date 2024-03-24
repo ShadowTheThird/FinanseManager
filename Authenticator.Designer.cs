f33c92051adcf850f72f951a564a04786720e7a9
ShadowTheThird <michal.walach111@gmail.com>
2024-03-24 16:32:26 +0100

    user authentication

---
+++ Authenticator.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace FinanseManager
+{
+    partial class Authenticator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.logon = new System.Windows.Forms.Button();
+            this.help = new System.Windows.Forms.Button();
+            this.password_label = new System.Windows.Forms.Label();
+            this.username_label = new System.Windows.Forms.Label();
+            this.register = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.username.Location = new System.Drawing.Point(162, 114);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(298, 26);
+            this.username.TabIndex = 0;
+            this.username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.password.Location = new System.Drawing.Point(162, 203);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(298, 26);
+            this.password.TabIndex = 1;
+            this.password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // logon
+            // 
+            this.logon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.logon.Location = new System.Drawing.Point(162, 297);
+            this.logon.Name = "logon";
+            this.logon.Size = new System.Drawing.Size(80, 31);
+            this.logon.TabIndex = 2;
+            this.logon.Text = "Log in";
+            this.logon.UseVisualStyleBackColor = true;
+            this.logon.Click += new System.EventHandler(this.logon_Click);
+            // 
+            // help
+            // 
+            this.help.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.help.Location = new System.Drawing.Point(380, 297);
+            this.help.Name = "help";
+            this.help.Size = new System.Drawing.Size(80, 31);
+            this.help.TabIndex = 3;
+            this.help.Text = "Help";
+            this.help.UseVisualStyleBackColor = true;
+            this.help.Click += new System.EventHandler(this.help_Click);
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.password_label.Location = new System.Drawing.Point(271, 180);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(86, 20);
+            this.password_label.TabIndex = 5;
+            this.password_label.Text = "Password";
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.username_label.Location = new System.Drawing.Point(271, 91);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(91, 20);
+            this.username_label.TabIndex = 6;
+            this.username_label.Text = "Username";
+            // 
+            // register
+            // 
+            this.register.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.register.Location = new System.Drawing.Point(261, 297);
+            this.register.Name = "register";
+            this.register.Size = new System.Drawing.Size(100, 31);
+            this.register.TabIndex = 7;
+            this.register.Text = "Register";
+            this.register.UseVisualStyleBackColor = true;
+            this.register.Click += new System.EventHandler(this.register_Click);
+            // 
+            // Authenticator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 458);
+            this.Controls.Add(this.register);
+            this.Controls.Add(this.username_label);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.help);
+            this.Controls.Add(this.logon);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Name = "Authenticator";
+            this.Text = "Log in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button logon;
+        private System.Windows.Forms.Button help;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.Button register;
+    }
+}
+
---
+++ Authenticator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinanseManager
+{
+    public partial class Authenticator : Form
+    {
+        public Authenticator()
+        {
+            InitializeComponent();
+        }
+
+        private void help_Click(object sender, EventArgs e)
+        {
+            Help_Page help_page = new Help_Page(this);
+            if (this.username.Text.Length > 0)
+            {
+                help_page.PreSet_Username(this.username.Text);
+            }
+            help_page.Show();
+            this.Hide();
+        }
+
+        private string LoadCSV(string name, string password)
+        {
+            string[] data = File.ReadAllLines("..\\..\\user_data.csv");
+            foreach (string line in data)
+            {
+                string[] split = line.Split(',');
+                if (split[0] == name)
+                {
+                    if (split[1] == password)
+                    {
+                        return split[3];
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void logon_Click(object sender, EventArgs e)
+        {
+            string user = LoadCSV(this.username.Text, this.password.Text);
+            if(user != null)
+            {
+                if(user == "0")
+                {
+                    Admin admin = new Admin();
+                    admin.Show();
+                    this.Hide();
+                }
+                Dashboard dashboard = new Dashboard(this, user);
+                dashboard.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void register_Click(object sender, EventArgs e)
+        {
+            Register register = new Register(this, this.username.Text);
+            register.Show();
+            this.Hide();
+        }
+    }
+}
